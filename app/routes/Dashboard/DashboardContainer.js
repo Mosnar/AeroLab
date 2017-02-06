b0b81de96f8ef2dd2187913cb3afb79ae5afae2b
Ransom <ranmantwo@gmail.com>
2017-02-05 19:11:38 -0500

    Cleaned up dashboard containers

--- app/routes/Dashboard/DashboardContainer.js
+++ app/routes/Dashboard/DashboardContainer.js
@@ -2,45 +2,22 @@ import React, {
     Component,
 } from 'react';
 
-import {connect} from 'react-redux';
-var ScrollableTabView = require('react-native-scrollable-tab-view');
-
 import Featured from './Featured';
 
-import {
-    getRecipes
-} from '../../actions/recipes';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 
-
-class FeaturedContainer extends Component {
+class DashboardContainer extends Component {
     constructor(props) {
         super(props);
     }
 
-    componentWillMount() {
-        this.props.getRecipes();
-    }
-
-
     render() {
         return (
             <ScrollableTabView tabBarPosition="bottom">
-                <Featured tabLabel="Featured" />
+                <Featured tabLabel="Featured"/>
             </ScrollableTabView>
         );
     }
 }
 
-
-FeaturedContainer.propTypes = {};
-
-function mapStateToProps(state) {
-    return {
-        recipes: state.recipes,
-    };
-}
-const actions = {
-    getRecipes
-};
-
-export default connect(mapStateToProps, actions)(FeaturedContainer);
+export default DashboardContainer;
