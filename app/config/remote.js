4b0370c3f6ff53535d9317b3ca9c13634ec1d43f
Ransom <ranmantwo@gmail.com>
2017-02-12 19:00:46 -0500

    Added environment config

--- app/config/remote.js
+++ app/config/remote.js
@@ -1,5 +1,11 @@
-const remote = {
+const dev = {
     API_URL: 'http://127.0.0.1/api/'
 };
+
+const prod = {
+    API_URL: 'http://sandstone.pw/api/'
+};
+
+const remote = (__DEV__) ? dev : prod;
 
 export default remote;
